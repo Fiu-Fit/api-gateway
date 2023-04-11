8f7aa4d51ab1fd64bd7c9226f5571d63352eac3e
Rocío <rplatini@fi.uba.ar>
2023-04-11 09:41:25 -0300

    Delete commented lines

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { LoggerFactory } from '@fiu-fit/common';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
-// import { firebaseApp } from './modules/auth/firebase';
 
 const logger = LoggerFactory('main');
 
@@ -16,25 +14,6 @@ async function bootstrap() {
 
   const port = parseInt(process.env.PORT || '8080');
   await app.listen(port);
-
-  // const email = 'test@gmail.com';
-  // const password = '123456';
-
-  // const auth = getAuth();
-  // createUserWithEmailAndPassword(auth, email, password)
-  //   .then((userCredential: any) => {
-  //     // Signed in
-  //     const user = userCredential.user;
-  //     logger.info('user: ', user);
-  //     // ...
-  //   })
-  //   .catch((error: any) => {
-  //     const errorCode = error.code;
-  //     logger.error('errorCode: ', errorCode);
-  //     const errorMessage = error.message;
-  //     logger.error('errorMessage: ', errorMessage);
-  //     // ..
-  //   });
 
   logger.info(`App is running on PORT: ${port}`);
 }
