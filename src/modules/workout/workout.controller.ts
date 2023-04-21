3090707fcf7640e704866a05e088c44aa5562883
Rocío <rplatini@fi.uba.ar>
2023-04-20 23:54:00 -0300

    Created controller functions

--- src/modules/workout/workout.controller.ts
+++ src/modules/workout/workout.controller.ts
@@ -1,8 +1,24 @@
-import { Controller, Inject, OnModuleInit, UseFilters } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  OnModuleInit,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 import { AllGlobalExceptionsFilter } from '../../shared/rpc-exceptions-filter';
 import {
   WORKOUT_SERVICE_NAME,
+  Workout,
+  WorkoutDto,
+  WorkoutList,
   WorkoutServiceClient,
 } from './interfaces/workout.pb';
 
@@ -17,5 +33,43 @@ export class WorkoutController implements OnModuleInit {
   public onModuleInit(): void {
     this.workoutService =
       this.client.getService<WorkoutServiceClient>(WORKOUT_SERVICE_NAME);
+  }
+
+  @Post('create')
+  create(
+    @Body() workout: WorkoutDto
+  ): Promise<Workout> | Observable<Workout> | Workout {
+    return this.workoutService.create(workout);
+  }
+
+  @Get()
+  findAll(): Observable<WorkoutList> {
+    return this.workoutService.findAll({});
+  }
+
+  @Delete(':id')
+  deleteById(
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<Workout> | Observable<Workout> | Workout {
+    return this.workoutService.deleteById({ id });
+  }
+
+  @Get(':id')
+  findById(
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<Workout> | Observable<Workout> | Workout {
+    return this.workoutService.findById({ id });
+  }
+
+  @Put(':id')
+  put(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() workout: WorkoutDto
+  ): Promise<Workout> | Observable<Workout> | Workout {
+    return this.workoutService.put({
+      id,
+      ...workout,
+      workout: undefined,
+    });
   }
 }
