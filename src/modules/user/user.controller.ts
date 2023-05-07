7e4448f2b0a82670191b9a823415d567f1fb033e
mmedela <mmedela@fi.uba.ar>
2023-05-07 16:35:04 -0300

    add search endpoint to gate way

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,10 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller, Injectable, UseGuards } from '@nestjs/common';
+import { Controller, Get, Injectable, Query, UseGuards } from '@nestjs/common';
+import { catchError, firstValueFrom } from 'rxjs';
+import { axiosErrorCatcher } from '../../shared/axios-error-catcher';
 import { ServerController } from '../../shared/server-controller';
 import { AuthGuard } from '../auth/auth.guard';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
 @UseGuards(AuthGuard)
@@ -9,5 +12,15 @@ import { AuthGuard } from '../auth/auth.guard';
 export class UserController extends ServerController {
   constructor(httpService: HttpService) {
     super(httpService, 'users');
+  }
+
+  @Get('search')
+  async searchUsers(@Query('query') query: string): Promise<User[]> {
+    const response = await firstValueFrom(
+      this.httpService
+        .get<User[]>(`/users/search?query=${query}`)
+        .pipe(catchError(axiosErrorCatcher))
+    );
+    return response.data;
   }
 }
