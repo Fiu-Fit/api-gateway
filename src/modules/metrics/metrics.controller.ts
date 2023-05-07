223692270e273e7b26f996c8e51ca3c2bf9baedd
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-06 23:06:00 -0300

    Added AuthGuard

--- src/modules/metrics/metrics.controller.ts
+++ src/modules/metrics/metrics.controller.ts
@@ -1,7 +1,9 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { ServerController } from '../../shared/server-controller';
+import { AuthGuard } from '../auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('metrics')
 export class MetricsController extends ServerController {
   constructor(httpService: HttpService) {
