8389d99193bc914554899c627298374084c5b7da
Rocío <rplatini@fi.uba.ar>
2023-05-16 11:37:20 -0300

    Register Ratings module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ExerciseModule } from './modules/exercise/exercise.module';
 import { GoalModule } from './modules/goals/goal.module';
 import { MetricsModule } from './modules/metrics/metrics.module';
+import { RatingModule } from './modules/ratings/rating.module';
 import { UserModule } from './modules/user/user.module';
 import { WorkoutModule } from './modules/workout/workout.module';
 
@@ -18,6 +19,7 @@ import { WorkoutModule } from './modules/workout/workout.module';
     UserModule,
     MetricsModule,
     GoalModule,
+    RatingModule,
   ],
   controllers: [AppController],
   providers:   [AppService],
